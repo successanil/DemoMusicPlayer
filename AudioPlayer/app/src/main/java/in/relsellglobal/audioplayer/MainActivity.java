9648a298a1983bc2a0f0a0b36a785fe7d6f92957
rootxces <kapilfrompokhri@gmail.com>
2017-10-27 17:22:59 +0530

    Splash Screen

--- AudioPlayer/app/src/main/java/in/relsellglobal/audioplayer/MainActivity.java
+++ AudioPlayer/app/src/main/java/in/relsellglobal/audioplayer/MainActivity.java
@@ -1,13 +1,46 @@
 package in.relsellglobal.audioplayer;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new splashEnd().execute();
+
+    }
+
+    public class splashEnd extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            for(int i=0;i<300;i++) {
+                // no task
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            intent = new Intent(MainActivity.this, navigation_activity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
