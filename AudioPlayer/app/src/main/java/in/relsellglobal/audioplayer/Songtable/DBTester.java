c036a2a62b61e8499c7a22f8a02168c3010a3f6a
Ashish0107 <ashish.rawat.matrix@gmail.com>
2017-11-02 17:52:02 +0530

    DBTsting

---
+++ AudioPlayer/app/src/main/java/in/relsellglobal/audioplayer/Songtable/DBTester.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017. Relsell Global
+ */
+
+package in.relsellglobal.audioplayer.Songtable;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by rahul on 2/11/17.
+ */
+
+public class DBTester extends AppCompatActivity {
+    DBHandler db;
+
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+       //setContentView(R.layout.activity_main);
+        db = new DBHandler(DBTester.this);
+
+        //new DBWriter().execute();
+
+        new DBReader().execute();
+    }
+    public class DBWriter extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Song song1 = new Song();
+            song1.setSongName("Feeling Good");
+            song1.setSongDescription("/storage/emulated/0/Download/feeling_good.mp3");
+            db.addSongData(song1);
+
+            Song song2 = new Song();
+            song2.setSongName("Sorry");
+            song2.setSongDescription("/storage/emulated/0/Download/feeling_good.mp3");
+            db.addSongData(song2);
+
+            return null;
+        }
+    }
+
+
+    public class DBReader extends AsyncTask<Void,Void,Song> {
+
+        @Override
+        protected Song doInBackground(Void... params) {
+            Song song = db.fetchSongData("/storage/emulated/0/Download/feeling_good.mp3");
+            return song;
+        }
+
+        @Override
+        protected void onPostExecute(Song song) {
+            super.onPostExecute(song);
+            Toast.makeText(DBTester.this,"Song for song No 1 is = "+song.getSongName(),Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
+}
+
+
